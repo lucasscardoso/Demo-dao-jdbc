6a32dda2e4bf437604ec1abe0f3172ebd0e8fe5b
gajeel redfox <lucas.silva.cardoso1996@gmail.com>
2024-03-26 16:42:08 -0300

    criado Department

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department depart = new Department(1,"teste");
+		
+		System.out.println(depart);
+
+	}
+
+}
